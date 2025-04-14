8b2bc2686f9081a40c6f8569c4c38f3f7c19cbdb
Jan Silva <janfrs3@gmail.com>
2025-04-14 12:07:38 +0200

    feat: Add Solution Checker

--- algorithms/01.hashing/01.hashmap/codility-4.frog_river_one/solution_test.go
+++ algorithms/01.hashing/01.hashmap/codility-4.frog_river_one/solution_test.go
@@ -1,55 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"reflect"
-	"regexp"
-	"strconv"
-	"strings"
+	"alghoritms.com/check"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-
-	var elementRegex = regexp.MustCompile(`"(?:[^"\\]|\\.)*"|\[.*?\]|[^,\[\]"]+`)
-
-	file, err := os.Open("test-input.txt")
-	if err != nil {
-		log.Fatal("Error opening:", err)
-		return
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	var parsed []interface{}
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		line = strings.TrimPrefix(line, "(")
-		line = strings.TrimSuffix(line, ")")
-
-		elements := elementRegex.FindAllString(line, -1)
-
-		for _, el := range elements {
-			val := parseValue(strings.TrimSpace(el))
-			parsed = append(parsed, val)
-		}
-	}
-	fmt.Printf("Parsed: %#v\n", parsed)
-
-	result := callFunction(Solution, parsed)
-	fmt.Printf("Input: %+v → Result: %v\n", parsed, result)
-
-	/*
-		for _, assertion := range testCases {
-			//fmt.Printf("%+v\n", assertion)
-			if (assertion.val) != assertion.expected {
-				t.Fatalf("Failed asserting that IsUnique for %+v", assertion)
-			}
-		}
-
-	*/
+	check.CheckSolution(t, Solution, "test-input.txt", "test-output.txt")
 }
--- algorithms/check/solution.go
+++ algorithms/check/solution.go
@@ -1,10 +1,14 @@
-package testing
+package check
 
 import (
+	"bufio"
+	"log"
+	"os"
 	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
+	"testing"
 )
 
 func callFunction(fn interface{}, args []interface{}) interface{} {
@@ -20,7 +24,6 @@ func callFunction(fn interface{}, args []interface{}) interface{} {
 }
 
 func parseValue(s string) interface{} {
-	// Quoted string
 	if strings.HasPrefix(s, "\"") && strings.HasSuffix(s, "\"") {
 		return strings.Trim(s, "\"")
 	}
@@ -59,31 +62,45 @@ func parseValue(s string) interface{} {
 	return s
 }
 
-func RunTests
-var elementRegex = regexp.MustCompile(`"(?:[^"\\]|\\.)*"|\[.*?\]|[^,\[\]"]+`)
+func parseValuesFile(inputFile string) [][]interface{} {
+	var elementRegex = regexp.MustCompile(`"(?:[^"\\]|\\.)*"|\[.*?\]|[^,\[\]"]+`)
 
-file, err := os.Open("test-input.txt")
-if err != nil {
-log.Fatal("Error opening:", err)
-return
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal("Error opening:", err)
+		return nil
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var inputs [][]interface{}
+	for scanner.Scan() {
+		var parsed []interface{}
+		line := strings.TrimSpace(scanner.Text())
+		line = strings.TrimPrefix(line, "(")
+		line = strings.TrimSuffix(line, ")")
+
+		elements := elementRegex.FindAllString(line, -1)
+
+		for _, el := range elements {
+			val := parseValue(strings.TrimSpace(el))
+			parsed = append(parsed, val)
+		}
+		inputs = append(inputs, parsed)
+	}
+	return inputs
 }
-defer file.Close()
 
-scanner := bufio.NewScanner(file)
-
-var parsed []interface{}
-for scanner.Scan() {
-line := strings.TrimSpace(scanner.Text())
-line = strings.TrimPrefix(line, "(")
-line = strings.TrimSuffix(line, ")")
-
-elements := elementRegex.FindAllString(line, -1)
-
-for _, el := range elements {
-val := parseValue(strings.TrimSpace(el))
-parsed = append(parsed, val)
+func CheckSolution(t *testing.T, fn interface{}, inputFile string,
+	outputFile string) {
+	inputs := parseValuesFile(inputFile)
+	outputs := parseValuesFile(outputFile)
+	for i, input := range inputs {
+		result := callFunction(fn, input)
+		expected := outputs[i][0]
+		if result != expected {
+			t.Errorf("Expected %s, got %s", expected, result)
+		}
+	}
 }
-}
-fmt.Printf("Parsed: %#v\n", parsed)
-
-result := callFunction(Solution, parsed)
